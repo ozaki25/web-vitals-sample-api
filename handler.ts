565d7d4553a90af4b8aea0a576910e8d59b28fc9
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-12 02:11:01 +0900

    put analytics to db

--- handler.ts
+++ handler.ts
@@ -1,14 +1,33 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
 import 'source-map-support/register';
+
+type AnalyticsTableType = {
+  name: string;
+  id: string;
+  value: number;
+  delta: number;
+};
 
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': '*',
 };
 
+const dynamo = new DynamoDB.DocumentClient();
+
+const putAnalytics = ({ name, id, value, delta }: AnalyticsTableType) => {
+  const params = {
+    TableName: process.env.ANALYTICS_TABLE,
+    Item: { name, id, value, delta },
+  };
+  return dynamo.put(params).promise();
+};
+
 export const analytics: APIGatewayProxyHandler = async event => {
   console.log(event.body);
-  const body = JSON.stringify(event.body);
+  const body = JSON.parse(event.body) as AnalyticsTableType;
+  await putAnalytics(body);
   return {
     statusCode: 200,
     headers,
