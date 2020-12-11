3d502ed8b01dbe9b79000f37bf92f4adbc66b705
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-12 01:48:19 +0900

    add dynamodb table config

--- serverless.ts
+++ serverless.ts
@@ -8,6 +8,7 @@ const serverlessConfiguration: AWS = {
       webpackConfig: './webpack.config.js',
       includeModules: true,
     },
+    defaultStage: 'dev',
   },
   // Add the serverless-webpack plugin
   plugins: ['serverless-webpack'],
@@ -16,12 +17,30 @@ const serverlessConfiguration: AWS = {
     runtime: 'nodejs12.x',
     region: 'ap-northeast-1',
     profile: 'aa',
+    stage: '${opt:stage, self:custom.defaultStage}',
     apiGateway: {
       minimumCompressionSize: 1024,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      DYNAMODB_TABLE: '${self:service}-${self:provider.stage}',
+      ANALYTICS_TABLE: '${self:provider.environment.DYNAMODB_TABLE}-analytics',
     },
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: [
+          'dynamodb:Query',
+          'dynamodb:Scan',
+          'dynamodb:GetItem',
+          'dynamodb:PutItem',
+          'dynamodb:UpdateItem',
+          'dynamodb:DeleteItem',
+        ],
+        Resource:
+          'arn:aws:dynamodb:${self:provider.region}:*:table/${self:provider.environment.DYNAMODB_TABLE}*',
+      },
+    ],
   },
   functions: {
     analytics: {
@@ -34,6 +53,32 @@ const serverlessConfiguration: AWS = {
           },
         },
       ],
+    },
+  },
+  resources: {
+    Resources: {
+      Analytics: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: '${self:provider.environment.ANALYTICS_TABLE}',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'name',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'name',
+              KeyType: 'HASH',
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        },
+      },
     },
   },
 };
